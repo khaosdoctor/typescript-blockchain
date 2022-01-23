eeb82562cdc5c021df48aece2c61f11338787e73
Lucas Santos <lhs.santoss@gmail.com>
2022-01-23 23:53:04 +0100

    refactor(logs): remove logs

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ let chain = blockchain.chain
 for (let i = 1; i <= 2; i++) {
   const block = blockchain.createBlock(`Block ${i}`)
   const mineInfo = blockchain.mineBlock(block)
-  // console.log(mineInfo)
   chain = blockchain.pushBlock(mineInfo.minedBlock)
 }
 
-console.log('CHAIN')
+console.log('--- GENERATED CHAIN ---\n')
 console.log(chain)
