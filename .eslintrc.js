d9f9bfd991ecb2b893bcaba530109a7e2800908d
Lucas Santos <lhs.santoss@gmail.com>
2022-01-23 16:00:04 +0100

    chore(configs): add config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
